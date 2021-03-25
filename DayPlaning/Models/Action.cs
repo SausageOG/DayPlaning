3871d43b91ea10ac55d7534a30e82851b6ca246e
SausageOG <sauageog@gmail.com>
2021-03-25 18:18:33 +0200

    Not so many changes but...  :
    1.Maybe finish view
    2.Creating vm and core models
    3.Connecting vm
    4.Starting view --> view  binding

---
+++ DayPlaning/Models/Action.cs
@@ -0,0 +1,9 @@
+﻿namespace DayPlaning.Models
+{
+    class Action
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ DayPlaning/Models/MainModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DayPlaning.Models
+{
+    class MainModel
+    {
+        private ObservableCollection<Action> _actions = new ObservableCollection<Action>();
+        public ReadOnlyObservableCollection<Action> Actions;
+
+        public MainModel()
+        {
+            Actions = new ReadOnlyObservableCollection<Action>(_actions);
+        }
+    }
+}
